7fc515450913622a6060a29b048d531bc3cb541c
jaquifigue23 <jaquifigueroa@ufm.edu>
2024-05-27 21:07:26 -0600

    todas las funcionalidades de ver partidas empatadas

--- src/gui_view.py
+++ src/gui_view.py
@@ -8,14 +8,19 @@ import pyautogui
 import webbrowser
 
 # Contador global para las capturas de pantalla
-screenshot_counter = 0
+screenshot_counter_ganadas = 0
+screenshot_counter_empatadas = 0
 
 # Inicializa el contador de capturas de pantalla al iniciar el programa
-screenshot_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'partidas_ganadas')
-if not os.path.exists(screenshot_directory):
-    os.makedirs(screenshot_directory)
-screenshot_counter = len([f for f in os.listdir(screenshot_directory) if f.startswith('partidaGanada_') and f.endswith('.jpg')])
+ganadas_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'partidas_ganadas')
+if not os.path.exists(ganadas_directory):
+    os.makedirs(ganadas_directory)
+screenshot_counter_ganadas = len([f for f in os.listdir(ganadas_directory) if f.startswith('partidaGanada_') and f.endswith('.jpg')])
 
+empatadas_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'partidas_empatadas')
+if not os.path.exists(empatadas_directory):
+    os.makedirs(empatadas_directory)
+screenshot_counter_empatadas = len([f for f in os.listdir(empatadas_directory) if f.startswith('partidaEmpatada_') and f.endswith('.jpg')])
 
 class TicTacToeGUI:
     def __init__(self, controller, menu_window):
@@ -25,14 +30,7 @@ class TicTacToeGUI:
         self.root.title("Tic Tac Toe")
         self.user_symbol = "O"
         
-        
         self.root.geometry('300x350+800+100')
-        #Evita la barra de cerrar, minimizar, maximizar
-        #self.root.overrideredirect(True)
-        
-        global screenshot_counter  # Referencia al contador global
-        self.screenshot_counter = screenshot_counter
-
         
         style = ttk.Style()
 
@@ -59,34 +57,34 @@ class TicTacToeGUI:
         self.root.focus_force()
         self.root.protocol("WM_DELETE_WINDOW", self.quit_game)
 
-        
-
     def on_button_click(self, row, col):
         if self.controller.board[row][col] == 0:  
             self.controller.make_user_move(row, col)  
             self.update_board(self.controller.board)  
             self.controller.make_computer_move()  
             self.update_board(self.controller.board)
-            #self.root.after(500, self.take_screenshot)
             result = self.check_game_status()  # Verificar el estado del juego después de cada movimiento
             if result:
                 self.show_message(result)
         else:
             messagebox.showwarning("Casilla Ocupada", "¡La casilla ya está ocupada! Intenta de nuevo.")
     
-    def take_screenshot(self):
-        global screenshot_counter  # Referencia al contador global
-        #desktop_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-        screenshots_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'partidas_ganadas')
-        #screenshot_path = os.path.join(desktop_path, f'tic_tac_toe_{screenshot_counter}.jpg')
+    def take_screenshot(self, empate=False):
+        global screenshot_counter_ganadas, screenshot_counter_empatadas  # Referencia a los contadores globales
+        folder_name = 'partidas_empatadas' if empate else 'partidas_ganadas'
+        screenshots_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), folder_name)
         
         # Crear la carpeta si no existe
         if not os.path.exists(screenshots_directory):
-           os.makedirs(screenshots_directory)
-        screenshot_path = os.path.join(screenshots_directory, f'partidaGanada_{screenshot_counter}.jpg')
-         
-        screenshot_counter += 1
+            os.makedirs(screenshots_directory)
         
+        if empate:
+            screenshot_path = os.path.join(screenshots_directory, f'partidaEmpatada_{screenshot_counter_empatadas}.jpg')
+            screenshot_counter_empatadas += 1
+        else:
+            screenshot_path = os.path.join(screenshots_directory, f'partidaGanada_{screenshot_counter_ganadas}.jpg')
+            screenshot_counter_ganadas += 1
+
         x = self.root.winfo_rootx()
         y = self.root.winfo_rooty()
         w = self.root.winfo_width()
@@ -94,9 +92,7 @@ class TicTacToeGUI:
 
         screenshot = pyautogui.screenshot(region=(x, y, w, h))       
         screenshot = screenshot.convert('RGB')
-        
         screenshot.save(screenshot_path)
-
 
     def update_board(self, board):
         for i in range(3):
@@ -112,7 +108,7 @@ class TicTacToeGUI:
         self.root.mainloop()
 
     def check_game_status(self):
-    # Método para verificar si el juego ha terminado
+        # Método para verificar si el juego ha terminado
         score = self.controller.check_game_status()
         if score is not None:
             if score == 10:
@@ -121,10 +117,9 @@ class TicTacToeGUI:
             elif score == -10:
                 return self.show_message("¡Has ganado Usuario!")
             elif score == 0:
+                self.root.after(1000, lambda: self.take_screenshot(empate=True))
                 return self.show_message("¡Empate!")
-        
 
-    # Función para salir del juego y regresar al menú principal
     def quit_game(self):
         self.root.destroy()  # Cerrar la ventana del juego
         self.menu_window.deiconify()
@@ -140,7 +135,6 @@ class MainMenu:
         style = ttk.Style()
         style.theme_use('clam')
 
-
         self.background_image = Image.open("background.jpg")
         self.background_photo = ImageTk.PhotoImage(self.background_image)
         self.background_label = tk.Label(self.root, image=self.background_photo)
@@ -155,8 +149,8 @@ class MainMenu:
         self.winning_history_button = ttk.Button(self.menu_frame, text="Ver Partidas Ganadas", command=self.show_winning_history, style="Rounded.TButton")
         self.winning_history_button.pack(pady=10)
 
-        self.losing_history_button = ttk.Button(self.menu_frame, text="Ver Partidas Empatadas", command=self.show_losing_history, style="Rounded.TButton")
-        self.losing_history_button.pack(pady=10)
+        self.draw_history_button = ttk.Button(self.menu_frame, text="Ver Partidas Empatadas", command=self.show_draw_history, style="Rounded.TButton")
+        self.draw_history_button.pack(pady=10)
         
         self.history_learning = ttk.Button(self.menu_frame, text="Historial de Aprendizaje", command=self.show_history_learnings, style="Rounded.TButton")
         self.history_learning.pack(pady=10)
@@ -184,38 +178,33 @@ class MainMenu:
         if choice:
             self.root.withdraw()  
             controller = TicTacToeController()  
-            gui = TicTacToeGUI(controller,self.root)  
+            gui = TicTacToeGUI(controller, self.root)  
             gui.start()  
 
     def show_winning_history(self):
-        #desktop_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
         screenshots_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'partidas_ganadas')
-
-        # screenshots = [f for f in os.listdir(desktop_path) if f.startswith('tic_tac_toe_') and f.endswith('.jpg')]
-        # if not screenshots:
-        #     messagebox.showinfo("Historial de Partidas Ganadas", "No hay capturas de pantalla registradas.")
-        # else:
-        #     self.show_screenshots(screenshots, desktop_path)
-
         screenshots = [f for f in os.listdir(screenshots_directory) if f.startswith('partidaGanada_') and f.endswith('.jpg')]
         if not screenshots:
            messagebox.showinfo("Historial de Partidas Ganadas", "No hay capturas de pantalla registradas.")
         else:
-            self.show_screenshots(screenshots, screenshots_directory)
+            self.show_screenshots(screenshots, screenshots_directory, "Ganada")
     
-    def show_screenshots(self, screenshots, path):
-        screenshots = sorted([f for f in os.listdir(path) if f.startswith('partidaGanada_') and f.endswith('.jpg')],key=lambda x: int(x.split('_')[1].split('.')[0]), reverse=True)
+    def show_draw_history(self):
+        screenshots_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'partidas_empatadas')
+        screenshots = [f for f in os.listdir(screenshots_directory) if f.startswith('partidaEmpatada_') and f.endswith('.jpg')]
+        if not screenshots:
+           messagebox.showinfo("Historial de Partidas Empatadas", "No hay capturas de pantalla registradas.")
+        else:
+            self.show_screenshots(screenshots, screenshots_directory, "Empatada")
+    
+    def show_screenshots(self, screenshots, path, tipo):
+        screenshots = sorted(screenshots, key=lambda x: int(x.split('_')[1].split('.')[0]), reverse=True)
         screenshot_window = tk.Toplevel(self.root)
-        screenshot_window.title("Historial de Partidas Ganadas")
-        screenshot_window.geometry('450x750+700+150')
-        screenshot_window.resizable(False,False)
-        
+        screenshot_window.title(f"Historial de Partidas {tipo}s")
+        screenshot_window.geometry('500x750+700+150')  # Aumenta el ancho para acomodar la scrollbar
 
-        container = tk.Frame(screenshot_window)
-        container.pack(fill=tk.BOTH, expand=True)
-
-        canvas = tk.Canvas(container)
-        scrollbar = tk.Scrollbar(container, orient="vertical", command=canvas.yview)
+        canvas = tk.Canvas(screenshot_window)
+        scrollbar = ttk.Scrollbar(screenshot_window, orient="vertical", command=canvas.yview)
         scrollable_frame = ttk.Frame(canvas)
 
         scrollable_frame.bind(
@@ -224,12 +213,12 @@ class MainMenu:
                 scrollregion=canvas.bbox("all")
             )
         )
+
         canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
         canvas.configure(yscrollcommand=scrollbar.set)
 
         canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
-
 
         for idx, screenshot in enumerate(screenshots):
             img = Image.open(os.path.join(path, screenshot))
@@ -237,50 +226,36 @@ class MainMenu:
             photo = ImageTk.PhotoImage(img)
             label = tk.Label(scrollable_frame, image=photo)
             label.image = photo  # Keep a reference to avoid garbage collection
-            label_text = tk.Label(scrollable_frame, text=f"Partida Ganada No.{idx + 1}", font=("Arial", 12))
+            label_text = tk.Label(scrollable_frame, text=f"Partida {tipo} No.{idx + 1}", font=("Arial", 12))
             label.grid(row=idx, column=0, padx=5, pady=5)
             label_text.grid(row=idx, column=1, padx=5, pady=5)
 
-
-    def show_losing_history(self):
-        try:
-            with open("historial_perdidas.txt", "r") as file:
-                messagebox.showinfo("Historial de Partidas Perdidas", file.read())
-        except FileNotFoundError:
-            messagebox.showinfo("Historial de Partidas Perdidas", "No hay partidas perdidas registradas.")
-    
     def show_history_learnings(self):
         top = tk.Toplevel(self.root)
         top.title("Historial Aprendizajes")
         top.geometry('500x500+700+150')
         self.directory = 'resultados'
         
-        #Creamos el TreeView
         tree = ttk.Treeview(top, columns=("Filename", "Action"), show="headings")
         tree.heading("Filename", text="Nombre del Archivo")
         tree.heading("Action", text="Double click")
-        tree.grid(row=1, column=1, padx=25, pady= 20, sticky='nsew')
+        tree.grid(row=1, column=1, padx=25, pady=20, sticky='nsew')
         
-        #Configuramos las columnas
         tree.column("Filename", width=350)
         tree.column("Action", width=100, anchor="center")
         
-        top.grid_rowconfigure(1,weight=1)
-        
-        #frame_buttons = ttk.Frame(top)
-        #frame_buttons.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
+        top.grid_rowconfigure(1, weight=1)
 
-        
         def load_files(directory):
             for filename in os.listdir(directory):
                 if filename.endswith(".pdf"):
-                    filepath = os.path.join(directory,filename)
-                    tree.insert("","end",values=(filename,"Open"), tags=(filepath,))
+                    filepath = os.path.join(directory, filename)
+                    tree.insert("", "end", values=(filename, "Open"), tags=(filepath,))
                     
         load_files(self.directory)
 
         def on_tree_select(event):
-            item =  tree.selection()[0]
+            item = tree.selection()[0]
             filepath = tree.item(item, "tags")[0]
             self.open_pdf(filepath)
         
@@ -288,9 +263,6 @@ class MainMenu:
     
     def open_pdf(self, file_path):
         webbrowser.open(file_path)
-    
-    
-        
 
     def show_team_members(self):
         team_members = """
@@ -305,7 +277,7 @@ class MainMenu:
 
 def start_menu():
     root = tk.Tk()
-    root.maxsize(1920,1080)
+    root.maxsize(1920, 1080)
     MainMenu(root)
     root.mainloop()
 
